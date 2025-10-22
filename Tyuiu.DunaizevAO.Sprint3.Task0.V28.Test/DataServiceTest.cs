e075c9a3044fd11c5baf634f3a0a31e98410e7f9
Sasha Dunaizev <jenderprolite@gmail.com>
2025-10-22 23:09:36 +0500

    Add project files.

---
+++ Tyuiu.DunaizevAO.Sprint3.Task0.V28.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.DunaizevAO.Sprint3.Task0.V28.Lib;
+
+namespace Tyuiu.DunaizevAO.Sprint3.Task0.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double value = 0;
+            int startValue = 1;
+            int stopValue = 17;
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
+            double wait = 131072;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.DunaizevAO.Sprint3.Task0.V28/Program.cs
@@ -0,0 +1,26 @@
+﻿using Tyuiu.DunaizevAO.Sprint3.Task0.V28.Lib;
+
+DataService ds = new DataService();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+Console.WriteLine("***************************************************************************");
+
+double value = 0.25;
+
+int startValue = 1;
+
+int stopValue = 17;
+
+
+Console.WriteLine("Переменная X: " + value);
+Console.WriteLine("Старт шага I: " + startValue);
+Console.WriteLine("Конец шага I: " + stopValue);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine(ds.GetMultiplySeries(value, startValue, stopValue));
+
+Console.ReadLine();
