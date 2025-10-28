c85d246b6d3ec3a553b90cac6ce30da500f4bf2b
Sasha Dunaizev <jenderprolite@gmail.com>
2025-10-28 22:52:46 +0500

    Task5

--- Tyuiu.DunaizevAO.Sprint3.Task5.V16.Lib/DataService.cs
+++ Tyuiu.DunaizevAO.Sprint3.Task5.V16.Lib/DataService.cs
@@ -11,10 +11,10 @@ namespace Tyuiu.DunaizevAO.Sprint3.Task5.V16.Lib
             {
                 for (int k = startValue2; k < stopValue2; k++)
                 {
-                    s += Math.Cos(k) + x * x;
+                    s += Math.Cos(k);
                 }
             }
-            return Math.Round(s, 3);
+            return Math.Round(s + x*x, 3);
         }
     }
 }
