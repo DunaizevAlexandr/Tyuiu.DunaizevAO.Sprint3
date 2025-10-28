4c33d24f4c5ee5107aa3e1f3ca075f0fe62f7a21
Sasha Dunaizev <jenderprolite@gmail.com>
2025-10-28 23:00:18 +0500

    Task5

--- Tyuiu.DunaizevAO.Sprint3.Task5.V16.Lib/DataService.cs
+++ Tyuiu.DunaizevAO.Sprint3.Task5.V16.Lib/DataService.cs
@@ -7,9 +7,9 @@ namespace Tyuiu.DunaizevAO.Sprint3.Task5.V16.Lib
         public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
         {
             double s = 0;
-            for (int i = startValue1; i < stopValue1; i++)
+            for (int i = startValue1; i <= stopValue1; i++)
             {
-                for (int k = startValue2; k < stopValue2; k++)
+                for (int k = startValue2; k <= stopValue2; k++)
                 {
                     s += (Math.Cos(k) + Math.Pow(x, 2));
                 }
