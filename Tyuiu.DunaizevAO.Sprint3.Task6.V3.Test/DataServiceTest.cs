543cb45d4f3a12bdd688f0a0cb3470925abd8f2b
Sasha Dunaizev <jenderprolite@gmail.com>
2025-10-28 23:39:08 +0500

    Task6

---
+++ Tyuiu.DunaizevAO.Sprint3.Task6.V3.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.DunaizevAO.Sprint3.Task6.V3.Lib;
+
+namespace Tyuiu.DunaizevAO.Sprint3.Task6.V3.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int a = 13;
+            int b = 19;
+            int wait = 129;
+            var res = ds.GetSumTheDivisors(a, b);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.DunaizevAO.Sprint3.Task6.V3/Program.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.DunaizevAO.Sprint3.Task6.V3.Lib;
+
+DataService ds = new DataService();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+Console.WriteLine("***************************************************************************");
+
+
+int startValue = 13;
+
+int stopValue = 19;
+
+
+Console.WriteLine("Старт шага: " + startValue);
+Console.WriteLine("Конец шага: " + stopValue);
+
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine(ds.GetSumTheDivisors(startValue, stopValue));
+
+Console.ReadLine();
