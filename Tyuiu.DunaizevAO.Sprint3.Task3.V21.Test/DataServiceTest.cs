f1b4ee1f7069dfd4288566f2b97eb72eb9acf0d4
Sasha Dunaizev <jenderprolite@gmail.com>
2025-10-28 20:43:29 +0500

    Task3

---
+++ Tyuiu.DunaizevAO.Sprint3.Task3.V21.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.DunaizevAO.Sprint3.Task3.V21.Lib;
+
+namespace Tyuiu.DunaizevAO.Sprint3.Task3.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string a = "f3g5ht g4j 34kg4";
+            char b = 'e';
+            string wait = "fegeht gej eekge";
+            var res = ds.ReplaceNumOnChar(a, b);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.DunaizevAO.Sprint3.Task3.V21/Program.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.DunaizevAO.Sprint3.Task3.V21.Lib;
+
+DataService ds = new DataService();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+Console.WriteLine("***************************************************************************");
+
+string value = "f3g5ht g4j 34kg4";
+char item = 'e';
+
+Console.WriteLine("Строка: " + value);
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine(ds.ReplaceNumOnChar(value, item));
+
+Console.ReadLine();
